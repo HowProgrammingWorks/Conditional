63d2427f3af7682bf7f587fc931377fce6ac382f
tshemsedinov <timur.shemsedinov@gmail.com>
2018-06-07 13:23:07 +0300

    Improve swith example

--- Javascript/6-switch.js
+++ Javascript/6-switch.js
@@ -1,35 +1,38 @@
 'use strict';
 
+// Use switch
+
 const getDay = (n) => {
   switch (n) {
-    case 1:
-      return 'Monday';
-    case 2:
-      return 'Tuesday';
-    case 3:
-      return 'Wednesday';
-    case 4:
-      return 'Thursday';
-    case 5:
-      return 'Friday';
-    case 6:
-      return 'Saturday';
-    case 7:
-      return 'Sunday';
-    default:
-      return ('Invalid number!');
+    case 1: return 'Monday';
+    case 2: return 'Tuesday';
+    case 3: return 'Wednesday';
+    case 4: return 'Thursday';
+    case 5: return 'Friday';
+    case 6: return 'Saturday';
+    case 7: return 'Sunday';
+    default: return new Error(`Invalid day number: ${n}`);
   }
 };
 
 console.log(getDay(2));
 
+// Use collection
 
-const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+const days = [
+  'Monday', 'Tuesday', 'Wednesday', 'Thursday',
   'Friday', 'Saturday', 'Sunday'
 ];
 
 const getDayWithoutSwitch = (n) => (
-  (n >= 1) && (n <= 7) ? days[n - 1] : 'Invalid number!'
+  n > 0 && n <= days.length ? days[n - 1] : new Error(`Invalid day number: ${n}`)
 );
 
+const getDayNumber = (name) => {
+  const n = days.indexOf(name);
+  return n !== -1 ? n : new Error(`Invalid day name: ${name}`);
+};
+
 console.log(getDayWithoutSwitch(2));
+console.log(getDayWithoutSwitch(20));
+console.log(getDayNumber('Sunday'));
