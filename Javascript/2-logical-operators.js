a7d55ee65506ac3edbce5d31cc1145b62ff931b9
Eugene Lepeico <03006jeka@gmail.com>
2018-08-26 11:34:18 +0300

    Add console outputs

--- Javascript/2-logical-operators.js
+++ Javascript/2-logical-operators.js
@@ -8,7 +8,7 @@ const beer = false;
 
 const goodDrinks = tea && juice; // true
 const badDrinks = vodka && beer; // false
-const badMix = vodka && juice;	// false
+const badMix = vodka && juice; // false
 
 console.log('...Logical operator AND (&&)...');
 
